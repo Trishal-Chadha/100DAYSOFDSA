11b6a327622231c0bae3c76303eb83067eb4c313
Trishal-Chadha <trishalchadha@gmail.com>
2026-02-01 22:45:26 +0530

    Add files via upload

---
+++ DAY1Q1.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+    int a[50];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    // shift elements
+    for (int i = n - 1; i >= pos - 1; i--) {
+        a[i + 1] = a[i];
+    }
+
+    // insert element
+    a[pos - 1] = x;
+
+    // print array
+    for (int i = 0; i < n + 1; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
