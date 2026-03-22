e0c8dc28b222f2fc2e081314d81bd00a005113d4
Trishal-Chadha <trishalchadha@gmail.com>
2026-03-22 14:37:24 +0530

    Add files via upload

---
+++ Day48q1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of Tree Node
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+// Function to count leaf nodes
+int countLeafNodes(struct TreeNode* root) {
+    // Base case
+    if (root == NULL)
+        return 0;
+
+    // If leaf node
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+
+    // Recursive calls
+    return countLeafNodes(root->left) + countLeafNodes(root->right);
+}
